e14ddddaf31e49110b21b976b3dcbdd94804268f
flipjack <rampzodia1@gmail.com>
2014-03-13 11:41:52 +0000

    Ultimos cambios

--- project/project/wsgi.py
+++ project/project/wsgi.py
@@ -5,16 +5,13 @@ from sys import path
 try:
 	SITE_ROOT = dirname(dirname(abspath(__file__)))
 	path.append(SITE_ROOT)
-
-
+	
 	# Tell wsgi to add the Python site-packages to its path. 
 	site.addsitedir('/home/grupodms/.virtualenvs/zaresdelaweb_blog/lib/python2.7/site-packages')
-
 	os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project.settings.produccion")
-
 	activate_this = os.path.expanduser("~/.virtualenvs/zaresdelaweb_blog/bin/activate_this.py")
 	execfile(activate_this, dict(__file__=activate_this))
-
+	
 	# Calculate the path based on the location of the WSGI script
 	project = '/home/grupodms/webapps/zaresdelaweb_blog/project/project/'
 	workspace = os.path.dirname(project)
